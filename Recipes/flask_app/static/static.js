278e931bd93bcfcb000302adfc2f42647f0b7cb3
Anthony Jameson <78007671+AJamesonan@users.noreply.github.com>
2022-12-21 13:01:51 -0500

    Delete Recipes directory

--- Recipes/flask_app/static/static.js
+++
@@ -1,7 +0,0 @@
-function getData(){
-    fetch('http://localhost:5000/get_data')
-        .then( response => response.json() )
-        .then( data => console.log(data) )
-}
-// Prints out { message : "Hello World" }
-getData();
